5f55ede113c5dc38ed4b1e33de28f254604c8030
oliviawyy <olivia.c@aluno.senai.br>
2026-02-20 08:10:58 -0300

    fazendo a função function erro

--- js/app.js
+++ js/app.js
@@ -1,196 +1,154 @@
- const v1 = document.getElementById("valor01")
- const v2 = document.getElementById("valor02")
+const v1 = document.getElementById("valor01")
+const v2 = document.getElementById("valor02")
 
- const resultado = document.getElementById("resultado")
+const resultado = document.getElementById("resultado")
 
 
-function calcularTotal(){
+function calcularTotal() {
     const v1 = parseFloat(document.getElementById('valor01').value);
     const v2 = parseFloat(document.getElementById('valor02').value);
 
     if (isNaN(v1) || isNaN(v2)) {
-        resultado.style.display = "flex"
-    
-        
-        resultado.innerHTML =
-        
-        `
-            <ul>
-                <li><span>ERRO</span></li>
-                <li>Voce deve preencher todos os campos!</li>       
-            </ul>
-        `; 
-        return null;
+       erro()
     }
-    
+
     resultado.style.display = "flex"
 
-    
+
     resultado.innerHTML =
-    
-    `
+
+        `
         <ul>
             <li><span>Total da Compra</span></li>
             <li>Voce esta adquirindo ${v2} unidades com valor unitário de ${v1}</li>      
-            <li>Valor total da compra: ${v1 * v2}</li>
+            <li>Valor total da compra: R$${v1 * v2}</li>
         </ul>
-    `;   
+    `;
 }
 
-function desconto(){
+function desconto() {
     const v1 = parseFloat(document.getElementById('valor01').value);
     const v2 = parseFloat(document.getElementById('valor02').value);
 
     if (isNaN(v1) || isNaN(v2)) {
-        resultado.style.display = "flex"
-    
-        
-        resultado.innerHTML =
-        
-        `
-            <ul>
-                <li><span>ERRO</span></li>
-                <li>Voce deve preencher todos os campos!</li>       
-            </ul>
-        `; 
-        return null;
+        erro()
     }
     const valorDesconto = (v1 * v2) / 100
-    
+
     resultado.style.display = "flex"
 
-    
+
     resultado.innerHTML =
-    
-    `
+
+        `
         <ul>
             <li><span>Desconto Aplicado</span></li>
             <li>Foi aplicado um desconto de ${v2} sobre o valor de ${v1} </li>       
             <li>Valor final com desconto: ${v1 - valorDesconto}%</li>
         </ul>
-    `;   
-    
+    `;
+
 
 
 
 }
 
-function juros(){
+function juros() {
     const v1 = parseFloat(document.getElementById('valor01').value);
     const v2 = parseFloat(document.getElementById('valor02').value);
 
-   
+
 
     if (isNaN(v1) || isNaN(v2)) {
-        resultado.style.display = "flex"
-    
-        
-            resultado.innerHTML =
-            
-            `
-                <ul>
-                    <li><span>ERRO</span></li>
-                    <li>Voce deve preencher todos os campos!</li>       
-                </ul>
-            `; 
-        return null;
+        erro()
     }
-    
+
     const valorJuros = (v1 * v2) / 100
     resultado.style.display = "flex"
 
-    
+
     resultado.innerHTML =
-    
-    `
+
+        `
         <ul>
             <li><span>Total da Compra</span></li>
             <li>Voce esta adquirindo ${v2} unidade(s) com valor unitário de ${v1}</li>       
-            <li>Valor total da compra: ${v1 + valorJuros}</li>
+            <li>Valor total da compra: R$${v1 + valorJuros}</li>
         </ul>
-    `;   
-    
+    `;
+
 }
 
 
-function comissao(){
+function comissao() {
     const v1 = parseFloat(document.getElementById('valor01').value);
     const v2 = parseFloat(document.getElementById('valor02').value);
     // pegando desconto
     const valorComissao = (v1 * v2) / 100
 
     if (isNaN(v1) || isNaN(v2)) {
-        resultado.style.display = "flex"
-    
-        
-        resultado.innerHTML =
-        
-        `
-            <ul>
-                <li><span>ERRO</span></li>
-                <li>Voce deve preencher todos os campos!</li>       
-            </ul>
-        `; 
-        return null;
+        erro()
     }
-    else{
-    
-    resultado.style.display = "flex"
+    else {
 
-    
-    resultado.innerHTML =
-    
-    `
+        resultado.style.display = "flex"
+
+
+        resultado.innerHTML =
+
+            `
         <ul>
             <li><span>Comissão Calculada</span></li>
             <li>Comissão de ${v2} sobre uma venda de ${v1} </li>      
             <li>Valor da comissão: ${valorComissao}%</li>
         </ul>
-    `; 
- } 
+    `;
+    }
 }
 
-function lucro(){
-        const v1 = parseFloat(document.getElementById('valor01').value);
-        const v2 = parseFloat(document.getElementById('valor02').value);
-        // pegando desconto
-        const valorLucro = v1 - v2
-    
-        if (isNaN(v1) || isNaN(v2)) {
-            resultado.style.display = "flex"
-    
-        
-            resultado.innerHTML =
-            
-            `
-                <ul>
-                    <li><span>ERRO</span></li>
-                    <li>Voce deve preencher todos os campos!</li>       
-                </ul>
-            `; 
-            return null;
-        }
-        else{
-        
+function lucro() {
+    const v1 = parseFloat(document.getElementById('valor01').value);
+    const v2 = parseFloat(document.getElementById('valor02').value);
+    // pegando desconto
+    const valorLucro = v1 - v2
+
+    if (isNaN(v1) || isNaN(v2)) {
+        erro()
+    }
+    else {
+
         resultado.style.display = "flex"
-    
-        
+
+
         resultado.innerHTML =
-        
-        `
+
+            `
             <ul>
                 <li><span>Lucro Obtido</span></li>
                 <li>Preço de venda: ${v1} | Custo: ${v2}</li>       
-                <li>Resultado financeiro: ${valorLucro}</li>
+                <li>Resultado financeiro: R$${valorLucro}</li>
             </ul>
-        `; 
-     } 
+        `;
+    }
 }
 
 
-function limpar(){
-    resultado.style.display='none';
+function erro() {
+    resultado.style.display = 'flex'
+    resultado.innerHTML =
+        `
+            <ul>
+                <li><span>ATENÇÃO</span></li>
+                <li>Você deve preencher todos os campos!</li>
+            </ul>
+            `
+}
+
+
+
+function limpar() {
+    resultado.style.display = 'none';
     v1.value = ''
     v2.value = ''
-    
+
 }
